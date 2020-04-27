b46bfbdb5177834e1288ab2df9d190e297c614b1
Benny <snk2kimo@gmail.com>
2020-04-27 17:02:27 +0800

    Updtae chat.py

--- chat.py
+++ chat.py
@@ -1,34 +1,38 @@
 # 75.程式練習 對話紀錄1 - 格式改寫
 # 讀取檔案
-lines = []
+
 def read_file(finename):
+	lines = []
 	with open(finename, 'r', encoding='utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
 	return lines
 
 # 轉換型態
-new = []
+
 def convert(lines):
+	new = []
+	person = None
 	for line in lines:
 		if line == 'Allen':
 			person = 'Allen'
 			continue
 		elif line == 'Tom':
 			person = 'Tom'
-		new.append(person + ': ' + line + '\n')
+		if person:
+			new.append(person + ': ' + line + '\n')
 	return new
 
 # 存入檔案
 def write_file(filename, new):
-	with open(filename, 'w') as f:
+	with open(filename, 'w' ) as f:
 		for line in new:
 			f.write(line)
 
 # 主程式
 def main():
-	read_file('input.txt') # 讀取檔案
-	convert(lines)
+	lines = read_file('input.txt') # 讀取檔案
+	new = convert(lines)
 	write_file('output.txt', new)
 
 main()
